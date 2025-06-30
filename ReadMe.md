99c8fbfc8721b05b706d7014b04fe3be733c7df6
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2025-06-30 08:29:52 -0700

    Implemented | Zigzag Conversion

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
 
 ## Medium
+* [Longest Palindromic Substring](LongestPalindromicSubstring/longest_palindromic_substring.py)
 * [Longest Substring Without Repeating Characters](LongestSubstringWithoutRepeatingCharacters/longest_substring_without_repeating_characters.py)
 
 * [Add Two Numbers](AddTwoNumbers/add_two_numbers.py)
---
+++ ZigzagConversion/zigzag_conversion.py
@@ -0,0 +1,26 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        rows = [[] for _ in range(numRows)]
+        
+        current_row = 0
+        going_down = False 
+
+        for char in s:
+            rows[current_row].append(char)
+            
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+            
+            if going_down:
+                current_row += 1
+            else:
+                current_row -= 1
+        
+        result = []
+        for row in rows:
+            result.extend(row)
+            
+        return "".join(result)
